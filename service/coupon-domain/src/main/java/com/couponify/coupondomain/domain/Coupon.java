2bf755b71446066f54d6139f7c8a97ab92e688ef
Arin Lee <linavell@naver.com>
2024-12-05 17:11:17 +0900

    feat(#7): 쿠폰 도메인 객체 생성
    - 쿠폰 도메인 객체 Coupon 클래스 구현
    - 쿠폰 상태를 나타내는 CouponStatus enum 구현
    - 쿠폰 수량을 관리하는 Quantity 클래스 구현

---
+++ service/coupon-domain/src/main/java/com/couponify/coupondomain/domain/Coupon.java
@@ -0,0 +1,13 @@
+package com.couponify.coupondomain.domain;
+
+public class Coupon {
+
+  private Long id;
+  private String name;
+  private CouponStatus status;
+  private Quantity quantity;
+
+  public Coupon() {
+  }
+
+}
---
+++ service/coupon-domain/src/main/java/com/couponify/coupondomain/domain/CouponStatus.java
@@ -0,0 +1,15 @@
+package com.couponify.coupondomain.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum CouponStatus {
+
+  AVAILABLE("발급 가능"), UNAVAILABLE("발급 불가");
+
+  private final String description;
+
+  CouponStatus(String description) {
+    this.description = description;
+  }
+}
