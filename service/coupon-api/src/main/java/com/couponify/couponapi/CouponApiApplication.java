ef7a2f3afef878d835b9a247b37652c9d57a432f
linavell <linavell@naver.com>
2024-12-06 17:39:36 +0900

    chore(#7): 공통으로 사용하지 않는 의존성 정리

--- service/coupon-api/src/main/java/com/couponify/couponapi/CouponApiApplication.java
+++ service/coupon-api/src/main/java/com/couponify/couponapi/CouponApiApplication.java
@@ -2,12 +2,8 @@ package com.couponify.couponapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.couponify")
-@EntityScan("com.couponify.coupondomain")
-@EnableJpaRepositories("com.couponify.coupondomain")
 public class CouponApiApplication {
 
   public static void main(String[] args) {
