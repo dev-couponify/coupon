68bfd2c9abcc50189384c27e5912c2a270ca5529
linavell <linavell@naver.com>
2024-12-09 14:21:25 +0900

    refactor(#9): CouponStatus enum 값 변경

--- service/coupon-domain/src/main/java/com/couponify/coupondomain/domain/coupon/CouponStatus.java
+++ service/coupon-domain/src/main/java/com/couponify/coupondomain/domain/coupon/CouponStatus.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 @Getter
 public enum CouponStatus {
 
-  AVAILABLE("발급 가능"), UNAVAILABLE("발급 불가");
+  ISSUABLE("발급 가능"), NOT_ISSUABLE("발급 불가");
 
   private final String description;
 
   CouponStatus(String description) {
     this.description = description;
+  }
+
+  public boolean isIssuable() {
+    return this.equals(ISSUABLE);
   }
 }
