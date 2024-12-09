aa560ee37ee3d6d74fe0f5ebe29d2577c6a9b45b
linavell <linavell@naver.com>
2024-12-09 20:50:26 +0900

    refactor(#9): CouponErrorCode 추가

--- service/coupon-api/src/main/java/com/couponify/couponapi/exception/CouponErrorCode.java
+++ service/coupon-api/src/main/java/com/couponify/couponapi/exception/CouponErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CouponErrorCode {
-  ;
+  COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 쿠폰입니다. : [%s]");
 
   private final HttpStatus status;
   private final String message;
