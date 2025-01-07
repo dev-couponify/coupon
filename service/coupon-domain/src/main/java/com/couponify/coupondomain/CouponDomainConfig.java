02a322d42d5322ed0c3ce9f40b7b521f56bc68ef
Arin Lee <linavell@naver.com>
2025-01-07 15:45:59 +0900

    chore(#20): 멀티 모듈 설정에 따른 애플리케이션 진입점 정리 및 모듈 스캔 설정 추가
    - 참조용 모듈에서 불필요한 애플리케이션 진입점 제거 및 이름 변경
    - 실행 가능한 api 모듈에서 domain 모듈을 스캔할 수 있도록 어노테이션 설정 추가

---
+++ service/coupon-domain/src/main/java/com/couponify/coupondomain/CouponDomainConfig.java
@@ -0,0 +1,10 @@
+package com.couponify.coupondomain;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class CouponDomainConfig {
+
+}
