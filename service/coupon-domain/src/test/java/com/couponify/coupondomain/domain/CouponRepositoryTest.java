2936cd720296d8f5f35ea2c3e500cfd24c58eb04
linavell <linavell@naver.com>
2025-01-09 15:54:19 +0900

    chore(#20): 테스트 코드에서 active profile을 test로 설정
    - 테스트 환경에서 H2 데이터베이스를 사용하도록 profile 설정 적용

--- service/coupon-domain/src/test/java/com/couponify/coupondomain/domain/CouponRepositoryTest.java
+++ service/coupon-domain/src/test/java/com/couponify/coupondomain/domain/CouponRepositoryTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.when;
 
 import com.couponify.coupondomain.domain.coupon.Coupon;
 import com.couponify.coupondomain.domain.coupon.repository.CouponRepository;
+import com.couponify.coupondomain.infrastructure.jpa.coupon.CouponRepositoryImpl;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -17,65 +18,71 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestConfig.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@Import(CouponRepositoryImpl.class)
 public class CouponRepositoryTest {
 
-  @Autowired
-  private CouponRepository couponRepository;
+    private final CouponRepository couponRepository;
 
-  @Test
-  @DisplayName("쿠폰을 저장할 수 있다.")
-  void saveCoupon() {
-    // given
-    Coupon coupon = CouponFixture.createCoupon();
+    @Autowired
+    public CouponRepositoryTest(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
 
-    // when
-    Coupon savedCoupon = couponRepository.save(coupon);
+    @Test
+    @DisplayName("쿠폰을 저장할 수 있다.")
+    void saveCoupon() {
+        // given
+        Coupon coupon = CouponFixture.createCoupon();
 
-    // then
-    assertNotNull(savedCoupon.getId());
-  }
+        // when
+        Coupon savedCoupon = couponRepository.save(coupon);
 
-  @Test
-  @DisplayName("저장된 쿠폰 ID로 쿠폰을 조회할 수 있다.")
-  void findCouponById() {
-    // given
-    Coupon coupon = CouponFixture.createCoupon();
-    Coupon savedCoupon = couponRepository.save(coupon);
+        // then
+        assertNotNull(savedCoupon.getId());
+    }
 
-    // when
-    Optional<Coupon> foundCoupon = couponRepository.findById(savedCoupon.getId());
+    @Test
+    @DisplayName("저장된 쿠폰 ID로 쿠폰을 조회할 수 있다.")
+    void findCouponById() {
+        // given
+        Coupon coupon = CouponFixture.createCoupon();
+        Coupon savedCoupon = couponRepository.save(coupon);
 
-    // then
-    assertTrue(foundCoupon.isPresent());
-    assertEquals(savedCoupon.getId(), foundCoupon.get().getId());
-  }
+        // when
+        Optional<Coupon> foundCoupon = couponRepository.findById(savedCoupon.getId());
 
-  @Test
-  @DisplayName("기한이 만료된 쿠폰 리스트를 조회할 수 있다.")
-  void findExpiredCoupons() {
-    // given
-    Coupon coupon = CouponFixture.createCoupon();
-    Coupon savedCoupon = couponRepository.save(coupon);
+        // then
+        assertTrue(foundCoupon.isPresent());
+        assertEquals(savedCoupon.getId(), foundCoupon.get().getId());
+    }
 
-    Clock mockClock = mock(Clock.class);
-    LocalDateTime expiredPeriod = LocalDateTime.now().plusDays(3);
-    when(mockClock.instant()).thenReturn(expiredPeriod.atZone(ZoneId.systemDefault()).toInstant());
-    when(mockClock.getZone()).thenReturn(ZoneId.systemDefault());
+    @Test
+    @DisplayName("기한이 만료된 쿠폰 리스트를 조회할 수 있다.")
+    void findExpiredCoupons() {
+        // given
+        Coupon coupon = CouponFixture.createCoupon();
+        Coupon savedCoupon = couponRepository.save(coupon);
 
-    // when
-    List<Coupon> expiredCoupons = couponRepository.findExpiredCoupons(LocalDateTime.now(mockClock));
+        Clock mockClock = mock(Clock.class);
+        LocalDateTime expiredPeriod = LocalDateTime.now().plusDays(3);
+        when(mockClock.instant()).thenReturn(
+            expiredPeriod.atZone(ZoneId.systemDefault()).toInstant());
+        when(mockClock.getZone()).thenReturn(ZoneId.systemDefault());
 
-    // then
-    assertFalse(expiredCoupons.isEmpty());
-    assertEquals(savedCoupon.getId(), expiredCoupons.get(0).getId());
-  }
+        // when
+        List<Coupon> expiredCoupons = couponRepository.findExpiredCoupons(
+            LocalDateTime.now(mockClock));
+
+        // then
+        assertFalse(expiredCoupons.isEmpty());
+        assertEquals(savedCoupon.getId(), expiredCoupons.get(0).getId());
+    }
 
 
 }
